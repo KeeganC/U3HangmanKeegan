3f99b83db99284bbdf2833cea48349aba0f0ee0e
KeeganC <36234333+KeeganC@users.noreply.github.com>
2018-04-19 15:13:33 -0400

    Add files via upload
    added all files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿/* Keegan Chan
+ * 4 19 2018
+ * U3HangmanKeegan
+ * simulates a game of hangman*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace U3HangmanKeegan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] sebisbad = new string[20];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        StreamReader streamReader = new StreamReader("wordList.txt");
+        
+
+        private void btnSolve_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGuess_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
