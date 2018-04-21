d975f2ca8949c74ed0bb8e0d500fd120e84deb49
KeeganC <36234333+KeeganC@users.noreply.github.com>
2018-04-21 17:38:06 -0400

    Add files via upload
    Updated to working version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,31 +25,108 @@ namespace U3HangmanKeegan
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Global variables
         Random random = new Random();
-        string strWordLineNumber = "";
+        private static string strWordLineNumber;
+        private static string strTargetWord;
+        string strBlanks = "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ";
+        int mistakeCounter = 0;
+        bool GameOver = false;
+        string strLettersGuessed = "";
+        string strShowWord = "";
 
         public MainWindow()
         {
             InitializeComponent();
-            SelectWord();
         }
 
-
+        // sets a word ready to be guessed
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
+            //picks the word from the list
             strWordLineNumber = random.Next(1, 15).ToString();
+            //NIU MessageBox.Show(strWordLineNumber);
+            SelectWord();
+            //NIU MessageBox.Show(strTargetWord);
+
+            //sets the number of spaces for letters
+            lblShowWord.Content = strBlanks.Substring(0, (strTargetWord.Length * 2) + 1);
+
+            //resets mistakes
+            mistakeCounter = 0;
+            GameOver = false;
+            bodyPart1.Visibility = Visibility.Hidden;
+            bodyPart2.Visibility = Visibility.Hidden;
+            bodyPart3.Visibility = Visibility.Hidden;
+            bodyPart4.Visibility = Visibility.Hidden;
+            bodyPart5.Visibility = Visibility.Hidden;
+            bodyPart6.Visibility = Visibility.Hidden;
+
+            //resets letters guessed
+            lblGuessed.Content = ":";
+
+            //assure that there are spaces where needed
+            if (strTargetWord.Contains(" "))
+            {
+                strShowWord = lblShowWord.Content.ToString();
+                lblShowWord.Content = strShowWord.Substring(0, (strTargetWord.IndexOf(" ") * 2) + 2) + " " + strShowWord.Substring((strTargetWord.IndexOf(" ") * 2) + 3);
+            }
         }
 
+        //guess the word
         private void btnSolve_Click(object sender, RoutedEventArgs e)
         {
+            if (txbWordInput.Text == strTargetWord)
+            {
+                if (GameOver == false)
+                {
+                    MessageBox.Show("Correct!\r\nPlease click reset to play again");
+                }
+                else
+                {
+                    MessageBox.Show("Solved after Game Over.\r\nClick reset to play again");
+                }
+            }
+            else
+            {
+                MessageBox.Show("incorrect, try again.");
 
+                //adding a body part if they make a mistake
+                MistakeMade();
+            }
         }
-
+        
+        //guessing a letter
         private void btnGuess_Click(object sender, RoutedEventArgs e)
         {
+            strShowWord = lblShowWord.Content.ToString();
 
+            //checks if the letter has been guessed before
+            strLettersGuessed = lblGuessed.Content.ToString();
+            if (strLettersGuessed.Contains(txbLetterInput.Text))
+            {
+                MessageBox.Show("already guessed");
+            }
+            else
+            {
+                //adds letter to letters guessed
+                lblGuessed.Content = lblGuessed.Content + " " + txbLetterInput.Text;
+
+                if (strTargetWord.Contains(txbLetterInput.Text))
+                {
+                    lblShowWord.Content = strShowWord.Substring(0, (strTargetWord.IndexOf(txbLetterInput.Text) * 2) + 2) + txbLetterInput.Text + strShowWord.Substring((strTargetWord.IndexOf(txbLetterInput.Text) * 2) + 3);
+                    strShowWord = lblShowWord.Content.ToString();
+                    lblShowWord.Content = strShowWord.Substring(0, (strTargetWord.LastIndexOf(txbLetterInput.Text) * 2) + 2) + txbLetterInput.Text + strShowWord.Substring((strTargetWord.LastIndexOf(txbLetterInput.Text) * 2) + 3);
+                }
+                else
+                {
+                    MessageBox.Show("incorrect, try again.");
+                    MistakeMade();
+                }
+            }
         }
 
+        //Method to select the word randomly from a list
         private static void SelectWord()
         {
             StreamReader streamReader = new StreamReader("wordList.txt");
@@ -59,11 +136,48 @@ namespace U3HangmanKeegan
                 while (!streamReader.EndOfStream)
                 {
                     string line = streamReader.ReadLine();
+                    if (line.Contains(strWordLineNumber + " "))
+                    {
+                        strTargetWord = line.Substring(strWordLineNumber.Length + 1); 
+                        streamReader.ReadToEnd();
+                    }
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+            }
+        }
+
+        //method for adding body parts when mistakes are made
+        private void MistakeMade()
+        {
+            mistakeCounter++;
+            if (mistakeCounter == 1)
+            {
+                bodyPart1.Visibility = Visibility.Visible;
+            }
+            if (mistakeCounter == 2)
+            {
+                bodyPart2.Visibility = Visibility.Visible;
+            }
+            if (mistakeCounter == 3)
+            {
+                bodyPart3.Visibility = Visibility.Visible;
+            }
+            if (mistakeCounter == 4)
+            {
+                bodyPart4.Visibility = Visibility.Visible;
+            }
+            if (mistakeCounter == 5)
+            {
+                bodyPart5.Visibility = Visibility.Visible;
+            }
+            if (mistakeCounter == 6)
+            {
+                bodyPart6.Visibility = Visibility.Visible;
+                MessageBox.Show("GAME OVER\r\nClick reset to try again");
+                GameOver = true;
             }
         }
     }
